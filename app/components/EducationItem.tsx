c58b7f87205947568cbfb34f7aedd7f1fce2b079
Edwald He <edwald@EdwalddeMacBook-Pro.local>
2024-12-03 10:49:08 +1100

    fix dark issue

--- app/components/EducationItem.tsx
+++ app/components/EducationItem.tsx
@@ -8,7 +8,7 @@ export default function EducationItem({
   return (
     <div className="border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 p-6">
       <h3 className="text-xl font-bold">{history.schoolName}</h3>
-      <div className="text-black font-bold text-sm">
+      <div className="text-black font-bold text-sm dark:text-white">
         {history.degree}, {history.major}
       </div>
       <div className="text-gray-400 text-sm">{history.timeSpan}</div>
